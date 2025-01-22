8f0d93899bb681e4e4ba73d5e6775cb2254763a7
Leonardo-snts <leonardosantos.ms77@gmail.com>
2025-01-22 13:53:51 -0400

    Criação repositorio

---
+++ noticias/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from django.db import models
+
+class Noticia(models.Model):
+    titulo = models.CharField(max_length=255)
+    conteudo = models.TextField()
+    autor = models.CharField(max_length=100)
+    data_publicacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
